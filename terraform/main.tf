c10144821f8e36150fcd9cf1a36be78bd0fbe0b2
andrii.gryshchenko <andrii.gryshchenko@globallogic.com>
2019-11-26 12:40:03 +0200

    Updated Readme Reverted TF bucket as env variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "TERRAFORM_S3_BUCKET" {}
-
-data "terraform_remote_state" "s3_state" {
-  backend = "s3"
-  config = {
-    bucket = var.TERRAFORM_S3_BUCKET
-    region = "us-east-2"
-    key = "redup/terraform.tfstate"
-  }
-}
-
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "re-dup-tf-backend"
+    key = "redup/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
